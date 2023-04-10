163cc9e00878cc64248f824f6bc94defcf3a6a04
Azat <aelchikushuulu@gmail.com>
2023-04-11 00:05:08 +0600

    exception

---
+++ src/main/java/com/github/aza0602/hw_2_5/exception/EmployeeAlreadyAddedException.java
@@ -0,0 +1,9 @@
+package com.github.aza0602.hw_2_5.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class EmployeeAlreadyAddedException extends RuntimeException{
+
+}
---
+++ src/main/java/com/github/aza0602/hw_2_5/exception/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.aza0602.hw_2_5.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException{
+}
